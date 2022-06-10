a928efe18cb395db8ffe50533ca7c891dd7b70ff
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-06-10 10:13:07 -0500

    added movement, grid boundaries and errors, coordinate display, steps display, and reset

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -13,38 +13,82 @@ const initialState = {
   steps: initialSteps,
 }
 
+const displayXY = ["(1, 1)", "(1, 2)", "(1, 3)",
+                     "(2, 1)", "(2, 2)", "(2, 3)",
+                     "(3, 1)", "(3, 2)", "(3, 3)"]
+
+const indexGrid = [[0, 1, 2],
+                  [3, 4, 5],
+                  [6, 7, 8]]
+
 export default class AppClass extends React.Component {
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
 
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
+  state = initialState;
 
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
+  getDisplayXY = index => {
+    return displayXY[index];
   }
 
   reset = () => {
-    // Use this helper to reset all states to their initial values.
+    this.setState(initialState)    
+  }
+
+  getYX = index => {
+    for(let y = 0; y < 3; y++){
+      for(let x = 0; x < 3; x++){
+        if(indexGrid[y][x] === index){
+          return [y, x];
+        }
+      }
+    }
   }
 
   getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+    const current = this.getYX(this.state.index);
+    const next = direction.map((each, index) => each + current[index]);
+
+    if(next[0] === -1){
+      return [indexGrid[current[0]][current[1]], "You can't go up"];
+
+    } else if(next[0] === 3){
+        return [indexGrid[current[0]][current[1]], "You can't go down"];
+
+    } else if(next[1] === -1){
+        return [indexGrid[current[0]][current[1]], "You can't go left"];
+
+    } else if(next[1] === 3){
+        return [indexGrid[current[0]][current[1]], "You can't go right"];
+
+    } else {
+      return [indexGrid[next[0]][next[1]], ""];
+    }
+
   }
 
-  move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+  move = (direction) => {
+    const [index, error] = this.getNextIndex(direction);
+    console.log(index, error);
+    if(error){
+      this.setState({
+        ...this.state,
+        message: error
+      })
+    } else {
+      this.setState({
+        ...this.state,
+        message: initialMessage,
+        index,
+        steps: this.state.steps + 1
+        
+      })
+    }
   }
 
-  onChange = (evt) => {
-    // You will need this to update the value of the input.
+  onChange = event => {
+    this.setState({
+      ...this.state,
+      email: event.target.value
+    })
   }
 
   onSubmit = (evt) => {
@@ -56,30 +100,36 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates {this.getDisplayXY(this.state.index)}</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}>
+                {idx === this.state.index ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={() => this.move([0, -1])}>LEFT</button>
+          <button id="up" onClick={() => this.move([-1, 0])}>UP</button>
+          <button id="right" onClick={() => this.move([0, 1])}>RIGHT</button>
+          <button id="down" onClick={() => this.move([1, 0])}>DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
           <input id="submit" type="submit"></input>
         </form>
       </div>
